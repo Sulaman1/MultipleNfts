c1aeffbf79adda7da603c09bcec59c112d0b8ee0
Sulaman1 <khansulaman303@gmail.com>
2021-10-09 04:04:07 +0500

    updated

--- contracts/AdvanceCollectibles.sol
+++ contracts/AdvanceCollectibles.sol
@@ -3,106 +3,25 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-//import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol"
 contract AdvanceCollectibles is ERC721 {
-    //pragma solidity >=0.6.0 <0.8.0;conLi
-    bytes32 internal keyHash;
-    uint256 internal fee;
     uint256 public randomResult;
     uint256 public tokenCounter;
-    address public LinkToken;
-    string public checkEx;
     uint256 public randNum;
-
-    enum Breed {
-        PUG,
-        PUG2,
-        SHIBA_INU,
-        ST_BERNARD
-    }
 
     event RequestedRandomness(bytes32 requestId);
     event RequestCollectible(bytes32 indexed requestId);
 
     mapping(uint256 => address) public requestIdToSender;
     mapping(uint256 => string) public requestIdToTokenURI;
-    mapping(uint256 => Breed) public tokenIdToBreed;
     mapping(uint256 => uint256) public requestIdToTokenId;
 
     constructor(
         address _vrfCoordinatorAddress,
         address _linkTokenAddress,
         bytes32 _keyHash
-    ) public ERC721("Doggies", "DOG") {
-        keyHash = _keyHash;
-        LinkToken = _linkTokenAddress;
-        fee = 1.0 * 10**18;
+    ) public ERC721("SportStars Punk", "SSP") {
         tokenCounter = 0;
     }
-
-    string public mockVar;
-    event myEvent(bytes32 requestId);
-
-    // function getrequestIdToSender(bytes32 requestId)
-    //     public
-    //     view
-    //     returns (address)
-    // {
-    //     return requestIdToSender[requestId];
-    // }
-
-    // function getrequestIdToTokenURI(bytes32 requestId)
-    //     public
-    //     view
-    //     returns (string memory)
-    // {
-    //     return requestIdToTokenURI[requestId];
-    // }
-
-    // function gettokenIdToBreed(uint32 tokenId) public view returns (Breed) {
-    //     return tokenIdToBreed[tokenId];
-    // }
-
-    // function gettokenIdToBreedStr(uint32 tokenId)
-    //     public
-    //     view
-    //     returns (string memory)
-    // {
-    //     Breed breed = Breed(tokenId);
-
-    //     if (Breed.PUG == breed) return "PUG";
-    //     if (Breed.SHIBA_INU == breed) return "SHIBA_INU";
-    //     if (Breed.ST_BERNARD == breed) return "ST_BERNARD";
-    // }
-
-    // function getrequestIdToTokenId(bytes32 requestId)
-    //     public
-    //     view
-    //     returns (uint256)
-    // {
-    //     return requestIdToTokenId[requestId];
-    // }
-
-    // function check4(string memory str) public payable returns (bytes32) {
-    //     require(
-    //         LINK.balanceOf(address(this)) > fee,
-    //         "Not enough LINK - fill contract with Link"
-    //     );
-    //     bytes32 requestId;
-    //     checkEx = str;
-
-    //     for (int256 i = 0; i < 4; i++) {
-    //         require(
-    //             LINK.balanceOf(address(this)) > fee,
-    //             "Not enough LINK - fill contract with Link"
-    //         );
-    //         requestId = requestRandomness(keyHash, fee);
-    //         requestIdToSender[requestId] = msg.sender;
-    //         requestIdToTokenURI[requestId] = str;
-    //         emit RequestCollectible(requestId);
-    //     }
-    //     return requestId;
-    // }
 
     function mint(string memory tokenURI) public payable {
         uint256 newItemId = tokenCounter;
@@ -121,28 +40,6 @@ contract AdvanceCollectibles is ERC721 {
         tokenCounter = tokenCounter + 1;
     }
 
-    /**
-     * Callback function used by VRF Coordinator
-     */
-    // function fulfillRandomness(bytes32 requestId, uint256 randomNumber)
-    //     internal
-    //     override
-    // {
-    //     address dogOwner = requestIdToSender[requestId];
-    //     string memory tokenURI = requestIdToTokenURI[requestId];
-    //     randNum = randomNumber;
-    //     uint256 newItemId = tokenCounter;
-
-    //     _safeMint(dogOwner, newItemId);
-    //     _setTokenURI(newItemId, tokenURI);
-
-    //     Breed breed = Breed(newItemId);
-    //     tokenIdToBreed[newItemId] = breed;
-    //     requestIdToTokenId[requestId] = newItemId;
-    //     tokenCounter = tokenCounter + 1;
-    //     //randomResult = randomNumber;
-    // }
-
     function setTokenURI(uint256 tokenId, string memory tokenURI) public {
         require(
             _isApprovedOrOwner(_msgSender(), tokenId),
@@ -150,19 +47,4 @@ contract AdvanceCollectibles is ERC721 {
         );
         _setTokenURI(tokenId, tokenURI);
     }
-
-    /**
-     * Requests randomness from a user-provided seed
-     */
-    // function getRandomNumber() public returns (bytes32 requestId) {
-    //     requestId = requestRandomness(keyHash, fee);
-    //     emit RequestedRandomness(requestId);
-    // }
-
-    // /**
-    //  * Requests the address of the Chainlink Token on this network
-    //  */
-    // function getChainlinkToken() public view returns (address) {
-    //     return address(LINK);
-    // }
 }
--- scripts/advance_collectible/create_collectible.js
+++ scripts/advance_collectible/create_collectible.js
@@ -1,26 +1,46 @@
-const AdvanceCollectiblesCont = artifacts.require("AdvanceCollectibles");
-//import truffle_config from '../../truffle-config.js';
+const { create } = require('ipfs-http-client')
 const Web3 = require("web3")
-const config = require("../../truffle-config.js");
-const helpFun = require("../helpful_scripts");
+
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 var fs = require('fs');
 const path = require("path");
 
+const client = create('https://ipfs.infura.io:5001/api/v0')
+
 //Changed to Rinkeby
+const priKey = 'b8ed812a73ca25905a534c4afc5b0f5ba2b387727cf73e4700fe843dcb7971b6';
 const kovanurl = 'https://rinkeby.infura.io/v3/c5a0caa6b6bc4b9783e5ef0f055aa538'
 const AdvanceCollectibles = require('../../build/contracts/AdvanceCollectibles.json');
 
-const sleep = (milliseconds) => {
-    console.log("Sleeping...")
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
+
+const metadataTemple = {
+    "name": "",
+    "description": "",
+    "image": "",
+};
+
+async function createSale(url) {
+    console.log("IN CREATE SALE: ", url);
+    const provider = new HDWalletProvider(priKey, kovanurl);
+    const web3 = new Web3(provider);
+    let myAccounts = await web3.eth.getAccounts();
+    const network = await web3.eth.net.getNetworkType();
+    const networkId = await web3.eth.net.getId();
+    const AdvanceCollectiblesData = await AdvanceCollectibles.networks[networkId];
+    const contAdd = AdvanceCollectiblesData.address;
+    const advanceCollectible = new web3.eth.Contract(AdvanceCollectibles.abi, AdvanceCollectiblesData.address);
+    const dev = myAccounts[0];
+    const receipt = await advanceCollectible.methods.mint(url).send({ from: dev });
+
+    let transactionReceipt = null
+    while (transactionReceipt == null) { // Waiting expectedBlockTime until the transaction is mined
+        console.log("TransactionHash : ", receipt.transactionHash)
+        transactionReceipt = await web3.eth.getTransactionReceipt(receipt.transactionHash);
+        //await sleep(expectedBlockTime)
+    }
 }
 
 module.exports = async callback => {
-
-    const priKey = 'b8ed812a73ca25905a534c4afc5b0f5ba2b387727cf73e4700fe843dcb7971b6';
-    console.log("Pri Key: ", priKey);
-
     const provider = new HDWalletProvider(priKey, kovanurl);
     const web3 = new Web3(provider);
 
@@ -38,53 +58,35 @@ module.exports = async callback => {
     const advanceCollectible = new web3.eth.Contract(AdvanceCollectibles.abi, AdvanceCollectiblesData.address);
     const dev = myAccounts[0];
     console.log("Dev Account: ", dev);
-    //const fromAdd = await web3.utils.toChecksumAddress('0xAaa25FB3d2b4617793Fa58fC1881F4bb76E6bd62');
-    const oldV = await advanceCollectible.methods.checkEx().call();
-    console.log("Old Value: ", oldV);
-
 
     //Reading ImageUrls from file
     var filePath = path.join(__dirname, '/IPFS SportStars Punk.txt')
     console.log(filePath)
     var text = fs.readFileSync(filePath, 'utf-8');
     var textByLine = text.split('\n')
-    console.log(textByLine[0]);
 
-    for (var i = 0; i < 10; i++) {
-        const receipt = await advanceCollectible.methods.mint(textByLine[i]).send({ from: dev });
-        let transactionReceipt = null
-        while (transactionReceipt == null) { // Waiting expectedBlockTime until the transaction is mined
-            console.log("TransactionHash : ", receipt.transactionHash)
-            transactionReceipt = await web3.eth.getTransactionReceipt(receipt.transactionHash);
-            //await sleep(expectedBlockTime)
+    for (var i = 0; i < textByLine.length; i++) {
+        console.log(textByLine[i]);
+        console.log("IPFS: ", textByLine[i], "  INDEX: ", i);
+
+        var name = 'SportsPunk ' + i.toString();
+        var description = 'Sports punk NFT';
+        var fileUrl = textByLine[i];
+        var url;
+        const data = JSON.stringify({
+            name, description, image: fileUrl
+        })
+
+        try {
+            const added = await client.add(data)
+            url = 'https://ipfs.infura.io/ipfs/' + added.path
+            console.log("Image URL : ", url);
+            await createSale(url)
+        }
+        catch (err) {
+            console.log(err);
         }
     }
 
-    console.log("First Sleep Over");
-
-    const newV = await advanceCollectible.methods.checkEx().call();
-    console.log("New Value: ", newV);
-
-    const results = await advanceCollectible.getPastEvents(
-        'RequestCollectible',
-        {
-            fromBlock: 0,
-            toBlock: 'latest'
-        }
-    )
-    console.log("Event Result : ", results)
-
-    // const requestId = results[0].returnValues.requestId;
-    // console.log("TxEvents: ", receipt)
-    // console.log("Results: ", results);
-    // console.log("Request Id: ", requestId);
-
-    // const tokenId = await advanceCollectible.methods.getrequestIdToTokenId(requestId).call();
-    // console.log("Token Id: ", tokenId);
-    // const breed = await advanceCollectible.methods.gettokenIdToBreedStr(tokenId).call();
-    // console.log("Breed is: ", breed);
-    // const myDogBreed = helpFun.get_breed(breed);
-    // console.log("MY Dog Breed ISSS: ", myDogBreed);
-
-    callback(results)
+    callback()
 }
